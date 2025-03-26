62567495d2eb5e215ebd95501a801f98064bae25
Ramo Alderweireld <alderweireld.ramo@gmail.com>
2025-03-27 00:44:29 +0100

    Update enabler-mk1.js

--- enabler-mk1.js
+++ enabler-mk1.js
@@ -3,7 +3,7 @@ export async function main(ns) {
     const target = ns.args[0];
     try {
         const growResult = await ns.grow(target);
-        ns.tprint(` Portfolio funds successfully invested: ${growResult}`);
+        ns.tprint(`Portfolio funds successfully invested: ${growResult}`);
     } catch (error) {
         ns.tprint(`Failed to successfully invest portfolio funds... ${target}: ${error}`);
     }
